eb3f0b4d78a5f62bc5a51d6ac0277083c142e7a9
David312 <davidhs312yt@gmail.com>
2019-04-20 14:17:21 +0200

    First part of HTTP module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from '@app/app.component';
+
+import { InMemoryDataService }  from '@app/services/in-memory-data/in-memory-data.service';
 
 import { HeroesComponent } from '@app/components/heroes/heroes.component';
 import { HeroDetailsComponent } from '@app/components/hero-details/hero-details.component';
@@ -21,7 +25,16 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   imports: [
 			BrowserModule,
 			FormsModule,
-			AppRoutingModule
+      AppRoutingModule,
+      HttpClientModule,
+      HttpClientInMemoryWebApiModule,
+
+      // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+      // and returns simulated server responses.
+      // Remove it when a real server is ready to receive requests.
+      HttpClientInMemoryWebApiModule.forRoot(
+        InMemoryDataService, { dataEncapsulation: false }
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
